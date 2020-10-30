c419327a8c937432cd18a8e8716a55f573fabc90
saharVac <vcsahar@gmail.com>
2020-10-30 08:42:45 -0400

    Handled all html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+// DEPENDENCIES
+var path = require("path")
+
+// ROUTING
+module.exports = function(app) {
+
+  // home page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+
+  // notes page
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  })
+
+  // For no matching route, direct to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
+const path = require("path")
 
 // Establish this as an express server
 const app = express()
@@ -8,10 +9,13 @@ const PORT = process.env.PORT || 8080;
 
 // Express handling data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json)
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // LINK (require) to route handlers
 // html routes
+require("./routes/htmlRoutes")(app)
 // API routes
 
 // Listener
